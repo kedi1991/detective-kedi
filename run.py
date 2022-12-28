389fa84982c3428c1414ecb7cd89ca4bead4f2de
kedi1991 <okurut.henry@yahoo.com>
2022-12-28 14:07:03 +0000

    Added greeting function and links to greeting text samples

--- run.py
+++ run.py
@@ -1,18 +1,42 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
 
 name = ""
+prompt_user = f"/n{name}>>>"
+prompt_computer = "Kedi>>>"
+
+GREET_FILE = "greetings.txt"
 
 
 def main():
     """
     Main function controlling access to all the other functions
     """
+    start()
+    greet()
+     
+
+def start():
+    """
+    Start the chat
+    """
     global name
-    print("Hi my name is Kedi. what is your name?")
+    print("Hi my name is Kedi. what is your name?\n")
     name = input(">>>")
+
+
+def greet():
+    """
+    Greet the participant
+    """
+    my_file = open(GREET_FILE)
+    content = my_file.read()
+    my_file.close()
     
+    greet_phrases = content.split("\n")
+    selected_greet_phrase = greet_phrases[random.randrange(len(greet_phrases))]
+
+    print(f"{selected_greet_phrase} {name} :)\n") 
+
 
 def positive_greet():
     """
